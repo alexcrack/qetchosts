b8436e1d370235283552e4d0cc56451d89f531fb
Alexey Avramchik <a13xcrack@gmail.com>
2019-03-20 14:30:10 +0300

    feat(settings): edit settings in settings window

--- src/settingsdialog.cpp
+++ src/settingsdialog.cpp
@@ -6,6 +6,19 @@ SettingsDialog::SettingsDialog(QWidget *parent) :
     ui(new Ui::SettingsDialog)
 {
     ui->setupUi(this);
+
+    mwSettings = settings.getWindowSettings();
+
+    ui->checkBoxMultipleInstances->setChecked(mwSettings.multipleInstances);
+    ui->checkBoxTrayIcon->setChecked(mwSettings.showTrayIcon);
+    ui->checkBoxMinimizeToTray->setChecked(mwSettings.minimizeToTrayIcon);
+
+    eSettings = settings.getEditorSettings();
+
+    ui->fontComboBox->setCurrentFont(eSettings.font);
+    ui->spinBoxFontSize->setValue(eSettings.fontSize);
+
+    ui->tabWidget->setCurrentIndex(0);
 }
 
 SettingsDialog::~SettingsDialog()
@@ -20,5 +33,47 @@ void SettingsDialog::on_pushButton_3_clicked()
 
 void SettingsDialog::on_pushButton_2_clicked()
 {
+    saveSettings();
+
     accept();
+}
+
+void SettingsDialog::on_checkBoxMultipleInstances_stateChanged(int arg1)
+{
+    mwSettings.multipleInstances = static_cast<bool>(arg1);
+}
+
+void SettingsDialog::on_checkBoxTrayIcon_stateChanged(int arg1)
+{
+    mwSettings.showTrayIcon = static_cast<bool>(arg1);
+
+    if (!mwSettings.showTrayIcon) {
+        on_checkBoxMinimizeToTray_stateChanged(0);
+        ui->checkBoxMinimizeToTray->setEnabled(false);
+    } else {
+        ui->checkBoxMinimizeToTray->setEnabled(true);
+    }
+}
+
+void SettingsDialog::on_checkBoxMinimizeToTray_stateChanged(int arg1)
+{
+    mwSettings.minimizeToTrayIcon = static_cast<bool>(arg1);
+}
+
+void SettingsDialog::on_fontComboBox_currentFontChanged(const QFont &f)
+{
+    eSettings.font = f;
+}
+
+void SettingsDialog::on_pushButton_4_clicked()
+{
+    saveSettings();
+}
+
+void SettingsDialog::saveSettings()
+{
+    settings.setWindowSettings(mwSettings);
+    settings.setEditorSettings(eSettings);
+
+    emit settingsChanged();
 }
--- src/settingsdialog.h
+++ src/settingsdialog.h
@@ -3,6 +3,8 @@
 
 #include <QDialog>
 
+#include "settings.h"
+
 namespace Ui {
 class SettingsDialog;
 }
@@ -15,13 +17,32 @@ public:
     explicit SettingsDialog(QWidget *parent = nullptr);
     ~SettingsDialog();
 
+signals:
+    void settingsChanged();
+
 private slots:
     void on_pushButton_3_clicked();
 
     void on_pushButton_2_clicked();
 
+    void on_checkBoxMultipleInstances_stateChanged(int arg1);
+
+    void on_checkBoxTrayIcon_stateChanged(int arg1);
+
+    void on_checkBoxMinimizeToTray_stateChanged(int arg1);
+
+    void on_fontComboBox_currentFontChanged(const QFont &f);
+
+    void on_pushButton_4_clicked();
+
+    void saveSettings();
+
 private:
     Ui::SettingsDialog *ui;
+    Settings settings;
+
+    Settings::MainWindowSettings mwSettings;
+    Settings::EditorSettings eSettings;
 };
 
 #endif // SETTINGSDIALOG_H
